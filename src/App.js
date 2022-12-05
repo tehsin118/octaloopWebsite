edcfabded89c73e40e2d4c635999bdd6daffca16
Muhammad Tehsin <tehsin@gmail.com>
2022-12-05 16:24:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Gateway from "./components/gateway/Gateway";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Gateway />
     </div>
   );
 }
---
+++ src/components/gateway/Gateway.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./gateway.css";
+const Gateway = () => {
+  return (
+    <div>
+      <section className="gateway">
+        <div className="row">
+          <div className="col-md-6 left">
+            <div className="left-content">
+              <div className="tag-line">
+                <h1>
+                  Where <span className="dream">Dreams </span>
+                </h1>
+                <h1>
+                  Come To <span className="reality">Reality.</span>
+                </h1>
+              </div>
+              <div className="detail">
+                Lorem ipsum dolor sit amet consectetur. Sit dolor malesuada hac
+                ac diam fames. A nunc augue scelerisque diam. Hac rhoncus eu
+                lorem tempus porta. Sit dictumst blandit proin interdum urna
+                aliquam in. Eros consectetur ut dolor ut. Risus sapien le ctus
+                condimentum aliquam diam imperdiet turpis.
+                <br />
+                Massa maecenas odio quam scelerisque phasellus at arcu nibh
+                mattis. Vitae vitae s ed senectus amet. Tincidunt blandit
+                consequat ultrices orci gravida diam mattis. Sed viverra eu eget
+                ac quam urna quam sed pulvinar. Nunc dui ac ornare pretium.
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 right">right</div>
+        </div>
+        ]
+      </section>
+    </div>
+  );
+};
+
+export default Gateway;
